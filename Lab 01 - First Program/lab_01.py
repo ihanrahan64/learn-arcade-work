59c77ff07565f34c7aff9c7df19ad4d48029bf47
ihanrahan64 <ih60676@email.vccs.edu>
2023-08-31 16:25:58 -0400

    work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-hi
+print("\"I see a little silhouetto of a man\"")
+print("Scaramouche, Scaramouche, will you do the Fandango?")
+print("Thunderbolt and lightning, very, very frightening me\"")
+print("\"Bohemian Rhapsody,\" by Queen")
