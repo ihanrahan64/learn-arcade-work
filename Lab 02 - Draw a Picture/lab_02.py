d0b83bf22e797b2f6d506cf7fa473500fd6f3711
ihanrahan64 <ih60676@email.vccs.edu>
2023-09-19 14:58:38 -0400

    Work on lab 2 and 3

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -1,60 +1,28 @@
 import arcade
 
-def the_moon(center_x, center_y):
-    arcade.draw_circle_filled(center_x, center_y, 50, (180, 180, 180))
-    arcade.draw_circle_filled(center_x-25, center_y, 50, arcade.color.DARK_MIDNIGHT_BLUE)
 
-#for the trees
-def tree(left, bottom):
-    arcade.draw_lrtb_rectangle_filled(left, left+30, bottom+20, bottom, arcade.color.KENYAN_COPPER)
-    arcade.draw_triangle_filled(left-26, bottom+19, left+15, bottom+151, left+51, bottom+19,arcade.color.BLACK)
-    arcade.draw_triangle_filled(left-25,bottom+20, left+15, bottom+150, left+50, bottom+20, arcade.color.DARK_GREEN)
-
-def star(center_x, center_y):
-    arcade.draw_circle_filled(center_x , center_y, 1, (240, 240 ,240))
-
-#Background
-arcade.open_window(800, 600, "d")
-arcade.set_background_color(arcade.color.DARK_MIDNIGHT_BLUE)
+arcade.open_window(600, 400, "Lab_2")
+arcade.set_background_color(arcade.color.BLUE_YONDER)
 arcade.start_render()
-arcade.draw_lrtb_rectangle_filled(0, 800, 400, 0, arcade.color.KOMBU_GREEN)
 
-#lake
-arcade.draw_ellipse_filled(600, 325, 750, 250, (183, 111, 85))
-arcade.draw_ellipse_filled(600, 325, 700, 200, (0, 31, 80))
-the_moon(700, 500-175)
-arcade.draw_circle_filled(675, 500-175, 50, (0, 31, 80))
-star(532, 300)
-star(512, 381)
-star(451, 338)
-star(600, 350)
-star(615, 250)
-star(325, 288)
-star(278, 400-65)
-star(351, 400-110)
+# Ground
+arcade.draw_lrtb_rectangle_filled(0, 600, 280, 0, arcade.color.BANGLADESH_GREEN)
 
-#sky
-arcade.draw_lrtb_rectangle_filled(0, 800, 600, 400, arcade.color.DARK_MIDNIGHT_BLUE)
-the_moon(700, 500)
-star(278, 465)
-star(532,500)
-star(325, 512)
-star(512, 419)
-star(215,540)
-star(351, 510)
-star(451, 468)
-star(145, 590)
-star(95, 450)
-star(60, 500)
-star(600, 450)
-star(615, 550)
+# River
+arcade.draw_circle_filled(1200, -100,  800, arcade.color.BLUE_SAPPHIRE)
+arcade.draw_circle_filled(1200, -100,  770, arcade.color.BANGLADESH_GREEN)
 
-# trees, a lot of trees
-for i in range(1, 10):
-    tree(70, 365 - (i * 74))
-    tree(12, 360 - (i *76))
-    tree(130, 345 - (i * 74))
+# Sky
+arcade.draw_lrtb_rectangle_filled(0, 600, 400, 280, arcade.color.BLUE_YONDER)
+arcade.draw_circle_filled(500, 350, 35, arcade.color.GOLD)
+
+# House
+arcade.draw_lrtb_rectangle_filled(200, 350, 190, 100, arcade.color.SKY_BLUE)
+arcade.draw_lrtb_rectangle_filled(220, 240, 140, 120, arcade.color.YELLOW)
+arcade.draw_lrtb_rectangle_filled(300, 320, 140, 120, arcade.color.YELLOW)
+arcade.draw_triangle_filled(175, 180, 275, 250, 375, 180, arcade.color.RADICAL_RED)
+arcade.draw_lrtb_rectangle_filled(300, 320, 140, 120, arcade.color.YELLOW)
+arcade.draw_lrtb_rectangle_filled(260, 280, 140, 100, arcade.color.RADICAL_RED)
 
 arcade.finish_render()
-
 arcade.run()
