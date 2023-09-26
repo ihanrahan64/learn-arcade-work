fc16ab0eea8941c157e525d3fe0936115e43e54b
ihanrahan64 <ih60676@email.vccs.edu>
2023-09-26 12:12:52 -0400

    Work on lab 3

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -1,5 +1,5 @@
 import arcade
-import random
+
 #moon
 def the_moon(center_x, center_y, red, green, blue):
     arcade.draw_circle_filled(center_x + 12, center_y, 78, (red + 5, green + 5, blue + 5))
@@ -18,46 +18,54 @@ def star(center_x, center_y):
     arcade.draw_circle_filled(center_x, center_y, 3, (15, 66, 217))
     arcade.draw_circle_filled(center_x , center_y, 1, (240, 240 ,240))
 
-# Background
-arcade.open_window(800, 600, "Lab_3")
-arcade.set_background_color(arcade.color.DARK_MIDNIGHT_BLUE)
-arcade.start_render()
-arcade.draw_lrtb_rectangle_filled(0, 800, 400, 0, arcade.color.KOMBU_GREEN)
+def main():
+    # Background
+    arcade.open_window(800, 600, "Lab_3")
+    arcade.set_background_color(arcade.color.DARK_MIDNIGHT_BLUE)
+    arcade.start_render()
+    arcade.draw_lrtb_rectangle_filled(0, 800, 400, 0, arcade.color.KOMBU_GREEN)
 
-# Lake
-arcade.draw_ellipse_filled(600, 325, 750, 250, (183, 111, 85))
-arcade.draw_ellipse_filled(600, 325, 700, 200, (0, 31, 80))
-the_moon(700, 500-175, 0, 31, 80)
-star(532, 300)
-star(512, 381)
-star(451, 338)
-star(600, 350)
-star(615, 250)
-star(325, 288)
-star(278, 400 - 65)
-star(351, 400 - 110)
+    # Lake
+    arcade.draw_ellipse_filled(600, 325, 750, 250, (183, 111, 85))
+    arcade.draw_ellipse_filled(600, 325, 700, 200, (0, 31, 80))
+    the_moon(700, 500-175, 0, 31, 80)
+    star(532, 300)
+    star(512, 381)
+    star(451, 338)
+    star(600, 350)
+    star(615, 250)
+    star(325, 288)
+    star(278, 400 - 65)
+    star(351, 400 - 110)
 
-# Sky
-arcade.draw_lrtb_rectangle_filled(0, 800, 600, 400, arcade.color.DARK_MIDNIGHT_BLUE)
-the_moon(700, 500, 0, 52, 102)
-star(278, 465)
-star(532,500)
-star(325, 512)
-star(512, 419)
-star(215,540)
-star(351, 510)
-star(451, 468)
-star(145, 590)
-star(95, 450)
-star(60, 500)
-star(600, 450)
-star(615, 550)
+    # Sky
+    arcade.draw_lrtb_rectangle_filled(0, 800, 600, 400, arcade.color.DARK_MIDNIGHT_BLUE)
+    the_moon(700, 500, 0, 52, 102)
+    star(278, 465)
+    star(532,500)
+    star(325, 512)
+    star(512, 419)
+    star(215,540)
+    star(351, 510)
+    star(451, 468)
+    star(145, 590)
+    star(95, 450)
+    star(60, 500)
+    star(600, 450)
+    star(615, 550)
 
-# trees, a lot of trees
-for i in range(1, 6):
-    tree(70, 365 - (i * 74))
-    tree(12, 360 - (i *76))
-    tree(130, 345 - (i * 74))
+    # trees, a lot of trees
+    for i in range(1, 15):
+        tree(70, 365 - (i * 74))
+        tree(12, 360 - (i *76))
+        tree(140, 345 - (i * 74))
+        tree(115 + (i * 111), 150)
+        tree(115 + (i * 90), 90)
+        tree(115 + (i * 95), 30)
+        tree(115 + (i * 100), -100)
 
-arcade.finish_render()
-arcade.run()
+    arcade.finish_render()
+    arcade.run()
+
+# calling main
+main()
